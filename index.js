ece71944e95ab543b6a8af2b4f55402b0f0e583b
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-02-06 23:14:19 -0300

    style and post request

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const haircutRouter = require('./routes/haircut-crud/haircut-crud');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -6,10 +8,9 @@ app.use(express.static('public'));
 app.set('views','./views');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('layout/layout');
-});
+app.use(bodyParser.json());
+app.use('/', haircutRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening to port http://localhost:${port}`);
+    console.log(`Example app listening to port http://localhost:${port}/home`);
 });
---
+++ routes/haircut-crud/haircut-crud.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const haircutRouter = express.Router();
+
+haircutRouter.get('/home', (req, res) => {
+    res.render('layout/layout');
+});
+
+haircutRouter.post('/haircut-data', (req,res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+module.exports = haircutRouter;

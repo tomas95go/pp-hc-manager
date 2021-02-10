0a3ea7f3672a48316a557866b9a5e03e74a8f980
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-02-09 23:04:20 -0300

    practicing sequelize

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const haircutRouter = require('./routes/haircut-crud/haircut-crud');
 const bodyParser = require('body-parser');
+const Haircut = require('./config/db');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.set('views','./views');
 app.set('view engine', 'pug');
+
+console.log('Index:', Haircut);
 
 app.use(bodyParser.json());
 app.use('/', haircutRouter);

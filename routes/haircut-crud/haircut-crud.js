74f8b816ac3236a47b85dfb6e2648707d5dbd324
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-02-20 18:58:29 -0300

    adding action buttons

--- routes/haircut-crud/haircut-crud.js
+++ routes/haircut-crud/haircut-crud.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const {
-    listAllHaircuts,
-    addNewHaircut,
-} = require('../../controllers/haircut-crud/haircut-crud');
+const { listAllHaircuts, addNewHaircut } = require('../../controllers/haircut-crud/haircut-crud');
 const haircutRouter = express.Router();
 
 haircutRouter.get('/home', (req, res) => {
@@ -18,6 +15,16 @@ haircutRouter.post('/haircut-data', (req, res) => {
     const { hcDescription, hcPrice } = req.body;
 
     addNewHaircut(hcDescription, hcPrice);
+});
+
+haircutRouter.put('/haircut-edit', (req, res) => {
+    const { id } = req.body;
+    console.log('Route, ID: ', id);
+});
+
+haircutRouter.put('/haircut-delete', (req, res) => {
+    const { id } = req.body;
+    console.log('Route delete, ID: ', id);
 });
 
 module.exports = haircutRouter;

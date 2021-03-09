3b4090d628d86036d6d5bc4ec9c2c1494a5d9921
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-08 23:34:29 -0300

    installing compression

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 const app = express();
 const port = 3000;
 
+app.use(compression());
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Example app listening to port http://localhost:${port}/home`);
+    console.log(`Process env, node_env: ${process.env.NODE_ENV}`);
 });

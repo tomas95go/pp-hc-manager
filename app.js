e8c37cae73c931fdb35013a1ea69c1863e15f902
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-08 23:39:17 -0300

    importing path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const path = require('path');
 const app = express();
 const port = 3000;
 

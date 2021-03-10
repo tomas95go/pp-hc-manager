3d3f23902d5323e2515e576bfd174eaa8eca79e9
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-10 00:18:25 -0300

    creating classes tests

--- tests/unit/classes/haircut.test.js
+++ tests/unit/classes/haircut.test.js
@@ -7,19 +7,3 @@ describe('Haircut class', () => {
         expect(newHaircut.price).toBeGreaterThan(0);
     });
 });
-
-//Goal: Create a haircut
-
-//New haircut attributes should be received from the client
-
-//Required attributes must not be null
-
-//Required attributes must not be undefined
-
-//Description should be a string
-
-//Price should be an integer number
-
-//Create a haircut object
-
-//Populate that haircut object with attributes

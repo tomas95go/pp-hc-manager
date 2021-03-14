3de1d396ceb516924c7e5c05b575a983fe530923
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-14 20:54:18 -0300

    testing with JEST

--- tests/unit/classes/haircut.test.js
+++ tests/unit/classes/haircut.test.js
@@ -1,34 +1,17 @@
-jest.mock('../../../classes/haircut');
 const Haircut = require('../../../classes/haircut');
 
 describe('Haircut class', () => {
-    beforeEach(() => {
-        const mConstructor = jest.fn((description, price) => {
-            return {
-                description,
-                price,
-            };
-        });
-
-        Haircut.mockImplementation(() => {
-            return {
-                constructor: mConstructor,
-            };
-        });
-    });
-
     test('A haircut should be an instance of class Haircut', () => {
         const a = new Haircut('Military', 1500);
-        console.log(a.mock);
+        expect(a).toBeInstanceOf(Haircut);
     });
-    /*
 
     test('A haircut object should contain the description and price properties', () => {
         const newHaircut = new Haircut('Military', 1500);
         expect(newHaircut).toHaveProperty('description');
         expect(newHaircut).toHaveProperty('price');
     });
-    
+
     test('A haircut should not have empty or undefined properties', () => {
         const newHaircut = new Haircut('Military', 10);
         expect(newHaircut.description).not.toBe(undefined);
@@ -42,5 +25,5 @@ describe('Haircut class', () => {
         expect(typeof newHaircut.price).toBe('number');
         const mockIsInteger = jest.fn(price => Number.isInteger(price));
         expect(mockIsInteger(15)).toBe(true);
-    });*/
+    });
 });

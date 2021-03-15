b44316748a35b57ba903034d8dcca78ca6e04328
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-15 18:00:31 -0300

    running migrations

---
+++ migrations/20210315204752-create-haircut.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Haircuts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            description: {
+                type: Sequelize.STRING,
+            },
+            price: {
+                type: Sequelize.INTEGER,
+            },
+            active: {
+                type: Sequelize.BOOLEAN,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Haircuts');
+    },
+};
--- models/haircut.js
+++ models/haircut.js
@@ -1,23 +1,25 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Haircut extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Haircut extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  };
-  Haircut.init({
-    description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Haircut',
-  });
-  return Haircut;
+    Haircut.init(
+        {
+            description: DataTypes.STRING,
+            price: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'Haircut',
+        }
+    );
+    return Haircut;
 };

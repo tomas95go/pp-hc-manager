98eb436ebd521c145b8737b789d9005cff97b477
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-22 14:11:43 -0300

    adding home route

--- routes/api/home.js
+++ routes/api/home.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/home', (req, res) => {
+    res.send('Working!');
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const haircutRoutes = require('./api/haircuts');
+const home = require('./api/home');
+
+router.use('/', home);
 
 router.use('/haircut', haircutRoutes);
 

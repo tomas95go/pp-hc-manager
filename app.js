ebc13c3211c246eae35d2be7f1aa1d7d1a7766e2
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-30 13:36:38 -0300

    fixing typo port for PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const path = require('path');
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT || 3000;
 const routes = require('./routes/index');
 
 app.use(compression());

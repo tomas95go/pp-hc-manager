d1a58ff0bf5ec2df40162e22e6263de3b470a933
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-31 13:53:07 -0300

    another attempt to run sequelize in heroku

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'development_heroku') {
+    require('dotenv').config();
+}
+
 module.exports = {
     development_local: {
         dialect: process.env.DB_DIALECT_DEV,

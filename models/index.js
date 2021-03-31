75340a2ce3854188f376b06cf2ce7c7ae501ae1f
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-03-30 21:40:16 -0300

    fixing development set up in config.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevLocal = process.env.NODE_ENV === 'development_local';
-const { development } = require('../config/config');
+const { development_local, development_heroku } = require('../config/config');
 const db = {};
 
 let sequelize;
